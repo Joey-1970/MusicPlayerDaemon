c31c9f0f423e55e7f9d13d6fd677d0b1677d4bec
Joey-1970 <paeper@horburg.de>
2022-02-05 20:30:28 +0100

    Update module.php

--- MusicPlayerDaemon/module.php
+++ MusicPlayerDaemon/module.php
@@ -197,7 +197,8 @@
 					break;
 				case "error":
 					$MessageValues = array_shift($MessageValue);
-					$MessageDisplay = implode(":", $MessageValues);
+					//$MessageDisplay = implode(":", $MessageValues);
+					$MessageDisplay = implode($MessageValues, ":");
 					$this->SendDebug("ReceiveData", "Fehler: ".$MessageDisplay, 0);
 					/*
 					If (isset($MessageValue[2])) {
@@ -262,13 +263,15 @@
 					break;
 				case "audio":
 					$MessageValues = array_shift($MessageValue);
-					$MessageDisplay = implode(":", $MessageValues);
+					//$MessageDisplay = implode(":", $MessageValues);
+					$MessageDisplay = implode($MessageValues, ":");
 					$this->SendDebug("ReceiveData", "Audio: ".$MessageDisplay, 0);
 					//$this->SendDebug("ReceiveData", "Audio: ".$MessageValue[1], 0);
 					break;
 				case "file":
 					$MessageValues = array_shift($MessageValue);
-					$MessageDisplay = implode(":", $MessageValues);
+					//$MessageDisplay = implode(":", $MessageValues);
+					$MessageDisplay = implode($MessageValues, ":");
 					$this->SendDebug("ReceiveData", "File: ".$MessageDisplay, 0);
 					/*
 					If (isset($MessageValue[2])) {
